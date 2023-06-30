efb5e3dd153a6ec0315a272c692a5aa7973ed5c4
Aryan Verma <119877909+Laneic@users.noreply.github.com>
2023-07-01 00:29:25 +0530

    Basic App Model Interaction
    Basic App Model Interaction

--- Server/App.py
+++ Server/App.py
@@ -1,11 +1,38 @@
 from flask import Flask
+import numpy as np
+from  tensorflow import keras
+from tensorflow.keras.models import model_from_json,
+from plantDisease import plantDiseaseClasses
 
 app = Flask(__name__)
+jsonFile = open("E:/Plant-Disease-Detection-System/model/trainedModel.json",'r')
+plantModelJson = jsonFile.read()
+plantModel = model_from_json(plantModelJson)
+plantModel.load_weights("E:/Plant-Disease-Detection-System/model/modelWeight.h5")
+jsonFile.close()
 
+def predictDisease():
 
+    
+    imagePath="E:/Plant-Disease-Detection-System/server/image.png"
+    newImg =keras.utils.load_img(imagePath, target_size=(256, 256))
+    testImage = keras.utils.img_to_array(newImg)
+    testImage = np.expand_dims(testImage, axis=0)
+    testImage = testImage/255
+    prediction = plantModel.predict(testImage)
+    index=prediction.argmax(axis=-1)[0]
+    className = plantDiseaseClasses[index] 
+
+    return className
 @app.route('/')
-def hello():
-    return"Hello, World!"
+def index():
+    return
+@app.route('/predict')
+def predict():
+    diseaseName=predictDisease()
+    return diseaseName
 
 if __name__ == '__main__':
     app.run()
+
+
