bbdc6cf568b7bba4e7f9f8f1f5292d3e25e380e7
Aryan Verma <119877909+Laneic@users.noreply.github.com>
2023-07-08 00:00:28 +0530

    Fixed Javascript

--- Client/index.js
+++ Client/index.js
@@ -1,28 +1,23 @@
 window.addEventListener('DOMContentLoaded', function() {
-    const imageUpload = document.getElementById('imageUpload');
     const previewImage = document.getElementById('preview');
-
     imageUpload.addEventListener('change', function() {
-      const file = this.files[0];
+    const file = this.files[0];
       if (file) {
-        const reader = new FileReader();
-
-        reader.addEventListener('load', function() {
-          previewImage.src = reader.result;
-        });
-
-        reader.readAsDataURL(file);
+          const reader = new FileReader();
+          reader.addEventListener('load', function() {
+            previewImage.src = reader.result;
+          });
+          reader.readAsDataURL(file);
       }
     });
-  });
-window.addEventListener("DOMContentLoaded", (event) => {
-    const el = document.getElementById('testing');
+    const el = document.getElementById('submit');
     if (el) {
       el.addEventListener('click',postImg);
     }
-})
+});
+
 async function postImg(){
-    const uploadedImage =document.getElementById('leafImage').files[0]
+    const uploadedImage =document.getElementById('imageUpload').files[0]
     const formData = new FormData();
     formData.append('formIMAGE', uploadedImage);
     
